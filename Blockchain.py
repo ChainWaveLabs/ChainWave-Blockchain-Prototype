2f8ecbdb59d59fce26fcc10b5063e9c683b21459
benjaminmbrown <benjaminbb@gmail.com>
2018-10-31 18:41:34 -0400

    add blockchain stubs

---
+++ Blockchain.py
@@ -0,0 +1,26 @@
+class Blockchain(object):
+
+    def __init__(self):
+        self.blockchain = [];
+        self.transactions = [];
+
+    def block(self):
+        #add new blocks to blockchain
+        pass
+
+    def transaction(self):
+        #add new tx to transactions
+        pass
+
+
+    def hash(block):
+        #block hashing
+        pass
+
+
+
+    def last_block(selfself):
+        #return last block in blockchain
+        pass
+
+
