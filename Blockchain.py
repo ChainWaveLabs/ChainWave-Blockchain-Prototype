ec20752152d1d5ea6972e20bcdb4d3a7e4119df2
benjaminmbrown <benjaminbb@gmail.com>
2018-10-31 18:59:43 -0400

    add tx logic + credits to van flymen

--- Blockchain.py
+++ Blockchain.py
@@ -4,23 +4,48 @@ class Blockchain(object):
         self.blockchain = [];
         self.transactions = [];
 
+    '''
+    A single block looks like
+    block = {
+        'index': 1,
+        'timestamp': 1506057125.900785,
+        'transactions': [
+            {
+                'sender': "8527147fe1f5426f9dd545de4b27ee00",
+                'recipient': "a77f5cdfa2934df3954a5c7c7da5df1f",
+                'amount': 5,
+            }
+        ],
+        'proof': 324984774000,
+        'previous_hash': "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"
+    }
+    '''
     def block(self):
         #add new blocks to blockchain
         pass
 
-    def transaction(self):
+    def transaction(self,sender,recipient,amt):
         #add new tx to transactions
-        pass
 
+        
+        self.transactions.append({
+            'sender':sender,
+            'recipient': recipient,
+            'amt': amt
+        })
+        #return the the next block # to be mined
+        return self.last_block['index'] + 1
 
+    @staticmethod
     def hash(block):
         #block hashing
         pass
 
-
-
+    @property
     def last_block(selfself):
         #return last block in blockchain
         pass
 
 
+###
+Example
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ChainWave-Blockchain-Prototype
 Prototype/demo blockchain from scratch. 
+
+
+
+Credit to [Daniel van Flymen](http://twitter.com/van_flymen) for his great overview "Learn Blockchains by Building One", which is the basis of this project.
